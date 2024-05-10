08ba52b91cbfc721971bf05bcafaa5f6c6abe536
bj-teacher <dantra2012@gmail.com>
2024-05-11 09:45:52 +1200

    change port listen

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/api/add-score', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
