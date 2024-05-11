200d6a48f29484fde3374a3b3d7071535ed51a9b
bj-teacher <dantra2012@gmail.com>
2024-05-12 09:52:04 +1200

    added options

--- index.js
+++ index.js
@@ -8,20 +8,25 @@ const app = express();
 
 const allowedOrigins = ['http://localhost:5173', 'https://bj-teacher-server-env-1.eba-n9at9mkt.ap-southeast-2.elasticbeanstalk.com'];
 
-app.use(cors({
+const corsOptions = {
     origin: function (origin, callback) {
         // allow requests with no origin (like mobile apps or curl requests)
         if (!origin) return callback(null, true);
         if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
+            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
             return callback(new Error(msg), false);
         }
         return callback(null, true);
     },
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-}));
-app.use(express.json());
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Ensure headers are explicitly allowed
+    credentials: true,  // If your API requires cookies or authentication headers
+    preflightContinue: false,
+    optionsSuccessStatus: 204  // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));  // Enable preflight across-the-board
 
 const pool = new Pool({
     user: process.env.DB_USER,
