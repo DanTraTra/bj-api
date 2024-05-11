1a2797adf83094421d63973723d3e223c50e9063
bj-teacher <dantra2012@gmail.com>
2024-05-11 12:46:02 +1200

    update multiple origin

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ const cors = require('cors');
 const {Pool} = require('pg');
 
 const app = express();
+
+const allowedOrigins = ['http://localhost:5173', 'https://bj-teacher-server-env-1.eba-n9at9mkt.ap-southeast-2.elasticbeanstalk.com'];
+
 app.use(cors({
-    origin: 'https://bj-teacher-server-env-1.eba-n9at9mkt.ap-southeast-2.elasticbeanstalk.com',
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
 app.use(express.json());
