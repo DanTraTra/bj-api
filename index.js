c566936705662c9ade19ba18064264d5dc3898d0
bj-teacher <dantra2012@gmail.com>
2024-05-17 06:55:48 +1200

    https.create

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const allowedOrigins = [
     'http://localhost:4173',
     'https://daaaaan.com',
     'https://api.daaaaan.com',
+    'https://bj-teacher-server-env-1.eba-n9at9mkt.ap-southeast-2.elasticbeanstalk.com',
+
 ];
 
 const corsOptions = {
