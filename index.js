33c57783d1f40c5bd25b450822b5b521d654ab2f
bj-teacher <dantra2012@gmail.com>
2024-05-17 08:31:04 +1200

    port 443

--- index.js
+++ index.js
@@ -88,7 +88,9 @@ app.post('/api/add-score', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 80;
+// const PORT = process.env.PORT || 443;
+const PORT = 443;
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
